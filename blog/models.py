bc167bc88da3e874743b7432e9fcfe8a88b2a436
Erfan <erfan9757@gmail.com>
2021-12-14 19:05:40 +0330

    set models of post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True,db_index=True)
+    content = models.TextField()
