b548128b5c3c1f6ac6e32d596823afbd37caf465
Erfan <erfan9757@gmail.com>
2021-12-14 19:34:05 +0330

    set models of Auther and set one to many relationship

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
 
-class post(models.Model):
+
+class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=250)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,db_index=True)
     content = models.TextField()
+    author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name="posts")
