8309df5f7c63028a0edb3c28fa83ff31905f688a
Erfan <erfan9757@gmail.com>
2021-12-14 20:58:21 +0330

    register model in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Author, Tag, Post
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
