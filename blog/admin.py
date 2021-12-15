3fd6644aa918ff223600d12ddd331234fe9a6a33
Erfan <erfan9757@gmail.com>
2021-12-15 22:34:44 +0330

    set slug and filter

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Author, Tag, Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "date" , "tag")
+    list_display = ("title", "date" , "author")
+    prepopulated_fields = {"slug" : ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
