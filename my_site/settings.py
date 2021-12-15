a6d1d0c3e0688396df8aaa86f586a155e741a53f
Erfan <erfan9757@gmail.com>
2021-12-15 22:07:43 +0330

    add str def for show on admin panel

--- my_site/settings.py
+++ my_site/settings.py
@@ -76,6 +76,8 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
