f66e6a94f3586cbad5636c1cde66f365331b7965
erfan-hamidi <erfan9757@gmail.com>
2021-12-18 12:48:59 +0330

    Complete database changes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 
 from blog.models import Post
 
@@ -15,20 +15,21 @@ def get_date(post):
 
 
 def starting_page(request):
-    latest_posts = Post.objects.all().order_by("date")[:3]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
       "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
       "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug = slug)
     return render(request, "blog/post-detail.html", {
       "post": identified_post
     })
