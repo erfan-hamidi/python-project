c99894bb9effdc33c6b3d03cbf4d42e34e1d77d4
erfan-hamidi <erfan9757@gmail.com>
2022-01-10 22:13:48 +0330

    add all post class

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.StartingPage.as_view(), name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
+    path("posts", views.Posts.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.post_detail,
          name="post-detail-page")  # /posts/my-first-post
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render , get_object_or_404
 
 from blog.models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView 
 
 all_posts = [
     
@@ -20,19 +20,19 @@ class StartingPage(ListView):
   template_name = "blog/index.html"
   modol = Post
   ordering = ["-date"]
+  context_object_name = "posts"
   
   def get_queryset(self):
       queryset = super().get_queryset()
       date = queryset[:3]
       return date
 
+class Posts(ListView):
+  template_name = "bblog/all-posts.html"
+  modol = Post
+  ordering = ["-date"]
+  context_object_name = "all_posts"
 
-
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-      "all_posts": all_posts
-    })
 
 
 def post_detail(request, slug):
